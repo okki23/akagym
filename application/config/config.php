c9b36ea5fb6af033ba73a8c95180729c3f5ce93b
okki23 <okkisetyawan@gmail.com>
2022-06-26 14:46:03 +0700

    tcpdf

--- application/config/config.php
+++ application/config/config.php
@@ -138,7 +138,7 @@ $config['subclass_prefix'] = 'MY_';
 | Note: This will NOT disable or override the CodeIgniter-specific
 |	autoloading (application/config/autoload.php)
 */
-$config['composer_autoload'] = FALSE;
+$config['composer_autoload'] = "vendor/autoload.php";
 
 /*
 |--------------------------------------------------------------------------
@@ -326,7 +326,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/userguide3/libraries/encryption.html
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = '4kAgym2022';
 
 /*
 |--------------------------------------------------------------------------
--- application/modules/member/controllers/Member.php
+++ application/modules/member/controllers/Member.php
@@ -69,7 +69,35 @@ class Member extends Parent_Controller {
        echo json_encode($getdata);   
   	}
   	  
-	 
+	  public function print()
+	  {
+		  $pdf = new \TCPDF();
+		  $pdf->AddPage('L', 'mm', 'A4');
+		  $pdf->SetFont('', 'B', 14);
+		  $pdf->Cell(277, 10, "DAFTAR PEGAWAI AYONGODING.COM", 0, 1, 'C');
+		  $pdf->SetAutoPageBreak(true, 0);
+		  // Add Header
+		  $pdf->Ln(10);
+		  $pdf->SetFont('', 'B', 12);
+		  $pdf->Cell(20, 8, "No", 1, 0, 'C');
+		  $pdf->Cell(100, 8, "Nama Pegawai", 1, 0, 'C');
+		  $pdf->Cell(120, 8, "Alamat", 1, 0, 'C');
+		  $pdf->Cell(37, 8, "Telp", 1, 1, 'C');
+		  $pdf->SetFont('', '', 12);
+		  $pegawai = $this->db->get('m_member')->result();
+		  $no=0;
+		  foreach ($pegawai as $data){
+			  $no++;
+			  $pdf->Cell(20,8,$no,1,0, 'C');
+			  $pdf->Cell(100,8,$data->nama,1,0);
+			  $pdf->Cell(120,8,$data->alamat,1,0);
+			  $pdf->Cell(37,8,$data->telp,1,1);
+		  }
+		  $pdf->SetFont('', 'B', 10);
+		  $pdf->Cell(277, 10, "Laporan Pdf Menggunakan Tcpdf, Instalasi Tcpdf Via Composer", 0, 1, 'L');
+		  $pdf->Output('Laporan-Tcpdf-CodeIgniter.pdf'); 
+	  }
+
 	public function get_data_edit(){
 		$id = $this->uri->segment(3);
 		$sql = $this->db->query('select *, CASE jenkel
